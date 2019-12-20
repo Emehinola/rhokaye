d62070eb82351f3d618b27afcb5704f024474d25
Emehinola Samuel <emehinolasam01@gmail.com>
2019-12-20 15:24:43 -0800

    settings updated

--- rhokaye/settings.py
+++ rhokaye/settings.py
@@ -131,9 +131,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 django_heroku.settings(locals())
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
