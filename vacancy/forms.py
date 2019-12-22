34da93f1ddd22e64537e746070ab512878a7e2eb
Emehinola Samuel <emehinolasam01@gmail.com>
2019-12-22 04:45:35 -0800

    models updated

--- vacancy/forms.py
+++ vacancy/forms.py
@@ -8,11 +8,11 @@ class Application(forms.ModelForm):
     first_name = forms.CharField(max_length=20)
     last_name = forms.CharField(max_length=30)
     post = forms.CharField(max_length=100)
-    qualifications = forms.CharField(max_length=100)
+    qualifications = forms.CharField(max_length=50)
     email_address = forms.CharField(max_length=100)
     phone = forms.CharField(max_length=20)
     years_of_experience = forms.IntegerField()
-    location = forms.CharField(max_length=200)
+    location = forms.CharField(max_length=100)
     created_at = models.DateField(auto_now_add=True)
 
 
--- vacancy/models.py
+++ vacancy/models.py
@@ -24,11 +24,11 @@ class Applications(models.Model):
     first_name = models.CharField(default="",max_length=20)
     last_name = models.CharField(default="",max_length=30)
     post = models.CharField(max_length=100)
-    qualifications = models.CharField(max_length=100)
-    email_address = models.CharField(max_length=100)
+    qualifications = models.CharField(max_length=50)
+    email_address = models.CharField(max_length=50)
     phone = models.CharField(default="",max_length=20)
     years_of_experience = models.IntegerField()
-    location = models.CharField(max_length=200)
+    location = models.CharField(max_length=100)
     created_at = models.DateField(auto_now=True)
 
     def __str__(self):
