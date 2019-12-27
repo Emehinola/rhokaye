5017593bc98662567844046581f42d24bf8ebc31
Emehinola Samuel <emehinolasam01@gmail.com>
2019-12-27 14:05:53 -0800

    model updated

--- vacancy/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-#from .models import Vacancy, Applications
-
-#admin.site.register(Vacancy)
-#admin.site.register(Applications)
--- vacancy/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url('vacancy/applications', views.application, name='application'),
-    url('', views.vacancy, name='vacancy')
-]
